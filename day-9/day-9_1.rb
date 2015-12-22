8b9741debaaf1f4fba85724e8a5120133f1fee71
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-22 18:03:27 +0000

    Complete Day 9 - Travelling Santa

---
+++ day-9/day-9_1.rb
@@ -0,0 +1,31 @@
+input = open 'input.txt'
+
+distances = {}
+
+input.each_line do |line|
+  next if line =~ /^\s+$/
+
+  place1, place2, distance = line.match(/(\w+) to (\w+) = (\d+)/).captures
+
+  distances[place1] ||= {}
+  distances[place1][place2] = distance.to_i
+  distances[place2] ||= {}
+  distances[place2][place1] = distance.to_i
+end
+
+shortest = 100_000_000
+
+distances.keys.permutation do |list|
+  start = list.shift()
+  distance = 0
+
+  list.each do |place|
+    distance += distances[start][place]
+    start = place
+  end
+
+  if distance < shortest
+    shortest = distance
+    puts "#{start}, #{list.join ', '}: #{distance}"
+  end
+end
