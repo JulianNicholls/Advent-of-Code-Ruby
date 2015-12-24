8cbba8c9cfb209dd89505ff557359558e2cbef05
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-24 19:46:09 +0000

    Complete day 13 - Delighted Diners

---
+++ day-13/day-13_2.rb
@@ -0,0 +1,45 @@
+require 'pp'
+
+input = File.read 'input.txt'
+
+happiness = {}
+
+input.each_line do |line|
+  next if line =~ /^\s+$/
+
+  name, dir, value, other = line.match(/(\w+) would (\w{4}) (\d{1,2}) (?:\w+\s){6}(\w+)./).captures
+
+  # puts "#{name}, #{dir}, #{value}, #{other}"
+  value = value.to_i
+
+  happiness[name] ||= {}
+  happiness[name][other] = dir == 'gain' ? value : -value
+  happiness[name]['julian'] = 0
+end
+
+# Add me in
+
+happiness['julian'] = happiness.keys.each_with_object({}) do |key, hash|
+  hash[key] = 0
+end
+
+pp happiness['julian']
+
+delight = 0
+
+diners = happiness.keys.size
+
+happiness.keys.permutation do |list|
+  mood = 0
+
+  (0...diners).each do |index|
+    guest = happiness[list[index]]
+    mood += guest[list[(index + 1) % diners]]
+    mood += guest[list[(index - 1) % diners]]
+  end
+
+  if mood > delight
+    puts "#{list.join ', '}: #{mood}"
+    delight = mood
+  end
+end
